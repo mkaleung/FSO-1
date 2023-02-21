f2c881ba011e23cba3d89efd8933d743510cc0d5
jincrypt <leung.mka@gmail.com>
2023-02-20 22:23:22 -0500

    Exercises 1.6-1.11 for Unicafe complete

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -25,7 +25,6 @@ const Statistics = (props) => {
       </div>     
     )
   }
-  console.log(props.data)
 
   const all = good + neutral + bad
   const average = ((good - bad)/all)
@@ -46,12 +45,12 @@ const Statistics = (props) => {
     </div>
   )
 }
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
 
   return (
     <div>
